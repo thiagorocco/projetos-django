1a64b3fa13c216d36e80466fbb9585922e63ca2f
thiagorocco <deroccotr@gmail.com>
2024-02-06 14:23:08 -0300

    Definição da classes models e criação dos modelos no banco de dados com makemigrations e migrate

--- controle-financeiro/financeiro/models.py
+++ controle-financeiro/financeiro/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Origem(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+
+class Orcamento(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    data = models.DateField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Lancamento(models.Model):
+    ENTRADA = 'e'
+    SAIDA = 's'
+    TIPO_OPERACAO_CHOICES = [(ENTRADA, 'Entrada'), (SAIDA, 'Saída'),]
+    data = models.DateField()
+    descricao = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    origem = models.ForeignKey(Origem, on_delete=models.CASCADE)
+    tipo_operacao = models.CharField(max_length=1,
+                                     choices=TIPO_OPERACAO_CHOICES)
