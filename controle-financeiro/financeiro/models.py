d3a5a949a2b6ebfb394586cb75293229ef6a790c
thiagorocco <deroccotr@gmail.com>
2024-02-06 16:16:17 -0300

    Implementação dos métodos __str__ nos models para facilitar a exibição no admin do django

--- controle-financeiro/financeiro/models.py
+++ controle-financeiro/financeiro/models.py
@@ -5,15 +5,24 @@ from django.db import models
 class Origem(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
 
 
 class Orcamento(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     data = models.DateField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
 
 
 class Lancamento(models.Model):
@@ -27,3 +36,7 @@ class Lancamento(models.Model):
     origem = models.ForeignKey(Origem, on_delete=models.CASCADE)
     tipo_operacao = models.CharField(max_length=1,
                                      choices=TIPO_OPERACAO_CHOICES)
+
+    def __str__(self):
+        return f"{self.data} | {self.descricao} | {self.valor} |" \
+            f"{self.categoria} | {self.origem} | {self.tipo_operacao}"
