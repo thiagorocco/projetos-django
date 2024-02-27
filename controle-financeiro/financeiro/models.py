59a47684f4e29e986425377eab498c50097c7e92
thiagorocco <deroccotr@gmail.com>
2024-02-26 21:59:52 -0300

    Alteração nas models Origem e Categoria, agora o campo nome de ambas possuirá valor único não podendo informar nomes repetidos

--- controle-financeiro/financeiro/models.py
+++ controle-financeiro/financeiro/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Origem(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.nome
 
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.nome
