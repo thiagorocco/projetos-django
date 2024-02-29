321304f09985d6fc303e66c417e10aa29caacc97
thiagorocco <deroccotr@gmail.com>
2024-02-29 06:28:56 -0300

    Inclusão de rotas para as views update_get_categoria e update_categoria

--- controle-financeiro/financeiro/urls.py
+++ controle-financeiro/financeiro/urls.py
@@ -4,7 +4,7 @@ from .views import lancamentos_save, rel_orcamentos, orcamentos_save
 from .views import rel_categorias, orcamentos, delete_lcto, update_get_lcto
 from .views import update_lcto, get_cotacao_dolar, rel_orcado_realizado
 from .views import delete_origem, delete_categoria, update_get_origem
-from .views import update_origem
+from .views import update_origem, update_get_categoria, update_categoria
 
 
 urlpatterns = [
@@ -24,9 +24,13 @@ urlpatterns = [
     path('delete_categoria/<int:id>/', delete_categoria,
          name='delete_categoria'),
     path('update_get_lcto/<int:id>/', update_get_lcto, name='update_get_lcto'),
+    path('update_get_categoria/<int:id>/', update_get_categoria,
+         name='update_get_categoria'),
     path('update_get_origem/<int:id>/', update_get_origem,
          name='update_get_origem'),
     path('update_lcto/<int:id>/', update_lcto, name='update_lcto'),
     path('update_origem/<int:id>/', update_origem, name='update_origem'),
+    path('update_categoria/<int:id>/', update_categoria,
+         name='update_categoria'),
     path('orc_real/', rel_orcado_realizado, name='rel_orcado_realizado'),
 ]
