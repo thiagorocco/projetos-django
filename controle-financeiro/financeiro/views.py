f503668f7857a0d2affb79f5684c768e9d7967d8
thiagorocco <deroccotr@gmail.com>
2024-03-11 21:04:58 -0300

    Ajuste na view rel_orcamentos para retornar tanto o filtro de categorias específicas como todas as categorias

--- controle-financeiro/financeiro/views.py
+++ controle-financeiro/financeiro/views.py
@@ -200,11 +200,17 @@ def rel_orcamentos(request):
 
     # Implemente os filtros aqui
     if get_dt_ini and get_dt_fim and get_cat:
-        orcamentos = Orcamento.objects.filter(
-            data_range=[get_dt_ini, get_dt_fim],
-            categoria_id=get_cat).order_by('data')
-        imprimir = True
-
+        cat = int(get_cat)
+        if cat == -1:
+            print('Entrou no -1')
+            orcamentos = Orcamento.objects.filter(data__range=[get_dt_ini, get_dt_fim]).order_by('data')
+            imprimir = True
+        else:
+            print('Entrou fora do -1')
+            orcamentos = Orcamento.objects.filter(
+                data__range=[get_dt_ini, get_dt_fim],
+                categoria__id=get_cat).order_by('data')
+            imprimir = True
     for orcamento in orcamentos:
         orcamento.nome_categoria = orcamento.categoria.nome
     return render(request, 'financeiro/rel_orcamentos.html',
