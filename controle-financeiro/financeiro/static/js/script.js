548d80fffe363191cfba12a51ffae6d954bfb927
thiagorocco <deroccotr@gmail.com>
2024-03-17 22:58:47 -0300

    Alteração no template base.html. O código javascript para marcar no menu da página que está em uso foi realocado no arquivo static/js/script.js. Agora no template é feita apenas a chamada da função que contém essa funcionalidade

--- controle-financeiro/financeiro/static/js/script.js
+++ controle-financeiro/financeiro/static/js/script.js
@@ -6,3 +6,34 @@ function mostrarMensagem(msg) {
         confirmButtonText: 'OK'
     });
 }
+
+function marcarPaginaSelecionada(){
+    var urlAtual = window.location.pathname;
+    var classeEspecifica = 'aselect';
+    var elementosAntigos = document.querySelectorAll('.' + classeEspecifica);
+    elementosAntigos.forEach(function(elementoAntigo) {
+        elementoAntigo.classList.remove(classeEspecifica);
+    });
+
+    var paginas = {
+        '/home/': 'nav1',
+        '/origens/': 'nav2',
+        '/categorias/': 'nav3',
+        '/orcamentos/': 'nav4',
+        '/rel-orctos/': 'nav5',
+        '/lancamentos/': 'nav6',
+        '/rel-lctos/': 'nav7',
+        '/orc_real/': 'nav8',
+
+    }
+    
+    for (var pagina in paginas){
+        if (urlAtual.indexOf(pagina) !== -1) {
+            var elementoNovo = document.getElementById(paginas[pagina]);
+            if (elementoNovo) {
+                elementoNovo.classList.add(classeEspecifica);
+            }
+            break
+        }
+    }    
+}
