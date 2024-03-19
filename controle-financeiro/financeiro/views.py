d56629d6d086af9c6b61248aa56800d8e239bf5a
Thiago de Rocco <71675039+thiagorocco@users.noreply.github.com>
2024-03-19 13:46:38 -0300

    Update views.py

--- controle-financeiro/financeiro/views.py
+++ controle-financeiro/financeiro/views.py
@@ -75,7 +75,8 @@ def lancamentos_save(request):
                     if saldo['origem__nome'] == novo_lcto.origem.nome:
                         if saldo['diferenca'] >= novo_lcto.valor:
                             novo_lcto.save()
-                            return redirect(reverse('rel_lancamentos'))
+                            messages.success(request, "Lançamento cadastrado com sucesso!")
+                            return redirect(reverse('lancamentos'))
                         else:
                             messages.error(request, f"Saldo insuficiente em {saldo['origem__nome']}")
                             return redirect(reverse('lancamentos'))
