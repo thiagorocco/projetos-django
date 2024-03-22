5d77a691930d15109058b6c46c7f8802fcb5ff59
Thiago de Rocco <71675039+thiagorocco@users.noreply.github.com>
2024-03-22 17:28:03 -0300

    Update services.py

--- controle-financeiro/financeiro/services.py
+++ controle-financeiro/financeiro/services.py
@@ -37,13 +37,9 @@ class Services:
             )
         return diferenca
 
-    def calcular_saldo_orc_realizado(self, data_ini, data_fim, categoria=None):
+    def calcular_saldo_orc_realizado(data_ini, data_fim, categoria=None):
         #data_ini = parse_date(data_ini)
         #data_fim = parse_date(data_fim)
-        
-        print('data ini: ', data_ini)
-        print('data Fim: ', data_fim)
-        print('Categoria: ', categoria)
         
         # Filtrar por categoria, se fornecida
         orcamentos = Orcamento.objects.filter(data__range=(data_ini, data_fim))
