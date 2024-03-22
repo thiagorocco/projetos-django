6fbb8a70eb6687003a0b5a937a3ccb0cc39f5176
Thiago de Rocco <71675039+thiagorocco@users.noreply.github.com>
2024-03-22 11:46:49 -0300

    Update views.py

--- controle-financeiro/financeiro/views.py
+++ controle-financeiro/financeiro/views.py
@@ -475,18 +475,18 @@ def rel_orcado_realizado(request):
         if data1 > data2:
             messages.error(request, "Data inicial deve ser menor que a data final")
             return redirect(reverse('rel_orcado_realizado'))
-        try:
-            cat = int(get_cat)
-            if cat == -1:
-                diferenca = Services.calcular_saldo_orc_realizado(get_dt_ini, get_dt_fim, None)
-            else:
-                diferenca = Services.calcular_saldo_orc_realizado(get_dt_ini, get_dt_fim, cat)
+        #try:
+        cat = int(get_cat)
+        if cat == -1:
+            diferenca = Services.calcular_saldo_orc_realizado(get_dt_ini, get_dt_fim, None)
+        else:
+            diferenca = Services.calcular_saldo_orc_realizado(get_dt_ini, get_dt_fim, cat)
             
-            sem_resultados = True if not orcamentos.exists() else False
-            imprimir = True
-        except TypeError as e:
-            messages.error(request, f"Preencha o formulário corretamente. Tipo de exceção: {e}")
-            return redirect(reverse('rel_orcado_realizado'))
+        imprimir = True
+        sem_resultados = len(diferenca) == 0
+        #except TypeError as e:
+        #    messages.error(request, f"Preencha o formulário corretamente. Tipo de exceção: {e}")
+        #    return redirect(reverse('rel_orcado_realizado'))
 
     return render(request, 'financeiro/orcado-realizado.html', {
                                 'categorias': categorias,
